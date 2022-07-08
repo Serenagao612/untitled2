4d3ff251fb8c1002714bf14ca475802b99f7f5b4
Jie Gao <gaoj16@yahoo.com>
2022-07-08 11:57:13 -0400

    Test and Practice array

--- src/TestAndPractice.java
+++ src/TestAndPractice.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 public class TestAndPractice {
 
     public static void main(String[] args) {
 
         System.out.println("Ni Hao");
+
+
+        int[] array={2,4,6,8,};
+        for (int i=0; i< array.length;i++){
+            System.out.println(array[i]+" ");
+        }
+
     }
 }
